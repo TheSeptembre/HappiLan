b93e39d33ae8dad0aafd5b12a8cab2d324a12e10
Exaha <45004927+Exaha@users.noreply.github.com>
2019-01-21 15:29:38 +0100

    Modif home.xml et home.kt

--- app/src/main/java/mspr/happilan/Home.kt
+++ app/src/main/java/mspr/happilan/Home.kt
@@ -1,6 +1,7 @@
 package mspr.happilan
 
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -10,13 +11,19 @@ import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_home.*
 
 class Home : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        fun resultContinue(view: View) {
-            var takePicIntent = Intent(this, CameraPreview::class.java)
-            startActivity(takePicIntent);
     }
 
+    fun employeContinue(view: View) {
+        var takePicIntent = Intent(this, CameraPreview::class.java)
+        startActivity(takePicIntent);
+
+    }
+
+    fun vigileContinue(view: View) {
+        var takePicIntent = Intent(this, CameraPreview::class.java)
+        startActivity(takePicIntent);
+    }
+}
