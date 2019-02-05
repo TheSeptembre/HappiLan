fe33e6d0e1bff12b85ec83a5b20134276b8b21b3
Tarkhagalle2080 <45005096+Tarkhagalle2080@users.noreply.github.com>
2019-02-05 15:20:38 +0100

    Modification Camera

---
+++ app/src/main/java/mspr/happilan/happilan/CameraActivity.java
@@ -0,0 +1,20 @@
+package mspr.happilan.happilan;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.TextureView;
+import android.widget.Button;
+
+public class CameraActivity extends AppCompatActivity {
+
+    private Button btnCapture;
+    private TextureView textureView;
+
+    protected void onCreatenCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_takepict);
+
+        
+
+    }
+}
